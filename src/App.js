cc3c07521b3b4c1476efe98e75f8bcaf7212a7af
Ryo-Shimizu86 <shimizu@1st-link.co.jp>
2022-04-19 13:03:42 +0900

    add first name input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello world</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="form-wrapper">
+          <h1>Create Account</h1>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className="firstName">
+              <label htmlFor="firstName">Fisrt Name</label>
+              <input
+                type="text"
+                className=""
+                placeholder="First Name"
+                name="firstName"
+                noValidate
+                onChange={this.handleChange}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
